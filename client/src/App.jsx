5b61ef70d80d0727d80c213408fe241620f58c5d
Michael Sanins <michael.sanins@gmail.com>
2021-03-08 14:24:22 +0000

    client-side React app installed

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Client-side works</h1>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// setup
 const express = require("express");
 const app = express();
 
@@ -7,6 +8,15 @@ dotenv.config({ path: "./config/config.env" });
 const connectDB = require("./config/db");
 connectDB();
 
+const cors = require("cors");
+app.use(cors());
+
+// http requests
+app.get("/", (req, res) => {
+  res.send("Backend works");
+});
+
+// server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
