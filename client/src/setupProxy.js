8243ec84aca9e2b0b15f66f1a31b41b1ca9a2066
Michael Sanins <michael.sanins@gmail.com>
2021-03-31 12:33:00 +0100

    test

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,23 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://localhost:5000",
-      changeOrigin: true,
-    })
-  );
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = function (app) {
+    app.use(
+      "/api",
+      createProxyMiddleware({
+        target: "https://skoob-site.herokuapp.com/",
+        changeOrigin: true,
+      })
+    );
+  };
+} else {
+  module.exports = function (app) {
+    app.use(
+      "/api",
+      createProxyMiddleware({
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      })
+    );
+  };
+}
