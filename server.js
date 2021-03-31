b6131ee2271f9ab726b693c2239641115121b80e
Michael Sanins <michael.sanins@gmail.com>
2021-03-31 11:59:33 +0100

    z

--- server.js
+++ server.js
@@ -24,12 +24,13 @@ app.use("/api/user", auth);
 
 // serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname + "client", "build", "index.html"));
   });
 }
 
+console.log(`Other thing: ${__dirname}`);
 console.log(`thing: ${process.env.NODE_ENV}`);
 // server
 const PORT = process.env.PORT || 5000;
