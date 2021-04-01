e4a26bba88ba958b6bbe6386232acb77c2740012
Michael Sanins <michael.sanins@gmail.com>
2021-04-01 13:49:52 +0100

    z

--- server.js
+++ server.js
@@ -24,9 +24,13 @@ app.use("/api/user", auth);
 
 // serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
-  app.get(/^((?!(api)).)*$/, (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "client/build")));
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
   });
 }
 
